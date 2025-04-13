73f437753c5ce1420e4b6f7f72e770b724eaef27
komathi2004 <tskomathi13@gmail.com>
2025-04-13 15:26:24 +0530

    made some changes in the front end and backend

--- models/Form.js
+++ models/Form.js
@@ -1,42 +1,25 @@
-// models/Form.js
 const mongoose = require('mongoose');
 
-const OptionSchema = new mongoose.Schema({
-  text: String
-});
-
-const FieldSchema = new mongoose.Schema({
-  type: {
-    type: String,
-    enum: ['text', 'paragraph', 'email', 'number', 'checkbox', 'radio'],
-    required: true
-  },
-  label: {
+const formSchema = new mongoose.Schema({
+  title: {
     type: String,
     required: true
   },
-  options: [OptionSchema],
-  selectedOptions: [Number],
-  required: {
-    type: Boolean,
-    default: false
-  }
-});
-
-const FormSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  fields: [FieldSchema],
+  fields: [{
+    id: String,
+    type: {
+      type: String,
+      enum: ['text', 'paragraph', 'email', 'number', 'checkbox', 'radio'],
+      required: true
+    },
+    label: String,
+    options: [String],
+    selectedOptions: [Number]
+  }],
   createdAt: {
     type: Date,
     default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
   }
 });
 
-module.exports = mongoose.model('Form', FormSchema);
+module.exports = mongoose.model('Form', formSchema);
